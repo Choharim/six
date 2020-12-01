0e95a30a5eb84f1132cc898df82fc7a5fc3bad70
Choharim <jhl8586@naver.com>
2020-12-02 07:49:43 +0900

    Add init,showRange,submitHandleFn

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ chooseUser = document.querySelector(".choose-user"),
 chooseMachine = document.querySelector(".choose-machine"),
 result = document.querySelector(".result");
 
+function showChoosingNumber(){}
+
+function submitHandle(event){
+  event.preventDefault();
+  if(isNaN(guessNumberInput.value)){
+    guessNumberInput.value="";
+  }else{
+    showChoosingNumber();
+  
+  }
+    
+
+}
+
 function showRange(){
   rangeTitle.innerText = `Generate a number between ${range.min} and ${range.value}`;
 }
 
 function init(){
-  range.addEventListener("change",showRange);
-  showRange();
+  setInterval(showRange,1000);
+  guessNumberContainer.addEventListener("submit",submitHandle);
 }
 init();
