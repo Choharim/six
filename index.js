0be881fdceeeb15d380fe2913847b9b69204c3b7
Choharim <jhl8586@naver.com>
2020-12-02 08:40:12 +0900

    Add makeRandomNumberFn

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ chooseUser = document.querySelector(".choose-user"),
 chooseMachine = document.querySelector(".choose-machine"),
 result = document.querySelector(".result");
 
+function makeRandomNumber(){
+  const numberOfRange = parseInt(range.value)+1;
+  let randomNumber = Math.floor(Math.random() *numberOfRange ); 
+  if(parseInt(range.value) === 0){
+    randomNumber = 0;
+  }
+  return randomNumber;
+}
+makeRandomNumber();
+setInterval(makeRandomNumber,1000);
+
 function showChoosingNumber(){
   chooseUser.innerText = `You chose: ${guessNumberInput.value}`;
   chooseMachine.innerText = `,the machine chose: `;
